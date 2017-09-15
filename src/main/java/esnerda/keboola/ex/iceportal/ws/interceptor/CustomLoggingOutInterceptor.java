4cc149bbc7e363aeb19bcab7d84dacec28aaa778
David Esner <david.esner@etnetera.cz>
2017-09-15 13:18:25 +0100

    UPDATE remove credentials from debug log

--- src/main/java/esnerda/keboola/ex/iceportal/ws/interceptor/CustomLoggingOutInterceptor.java
+++ src/main/java/esnerda/keboola/ex/iceportal/ws/interceptor/CustomLoggingOutInterceptor.java
@@ -1,10 +1,19 @@
 package esnerda.keboola.ex.iceportal.ws.interceptor;
 
+import java.io.ByteArrayInputStream;
 /**
  * @author David Esner
  */
 import java.io.OutputStream;
+import java.io.StringWriter;
 import java.time.Instant;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.interceptor.LoggingOutInterceptor;
@@ -14,9 +23,11 @@ import org.apache.cxf.io.CachedOutputStreamCallback;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.Phase;
 import org.apache.logging.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 public class CustomLoggingOutInterceptor extends LoggingOutInterceptor {
-	
+
 	private Logger log;
 
 	public CustomLoggingOutInterceptor(Logger log) {
@@ -34,20 +45,41 @@ public class CustomLoggingOutInterceptor extends LoggingOutInterceptor {
 	}
 
 	public class LoggingCallback implements CachedOutputStreamCallback {
+		private static final String AUTH_HEADER = "ICEAuthHeaderWithMType";
+
 		public void onFlush(CachedOutputStream cos) {
 		}
 
 		public void onClose(CachedOutputStream cos) {
 			try {
 				StringBuilder builder = new StringBuilder();
-				builder.append("Logging outbound message: \n");
-				builder.append(Instant.now() + ": ");
 				cos.writeCacheTo(builder);
-				String soapXml = builder.toString();				
-				System.out.println(soapXml);
+				String soapXml = removeCredentials(builder.toString());
+				soapXml = "Logging outbound message: \n" + Instant.now() + ": " + soapXml;
+				log.info(soapXml);
 			} catch (Exception e) {
 				log.error(e);
 			}
 		}
+
+		private String removeCredentials(String message) throws Exception {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(new ByteArrayInputStream(message.getBytes()));
+			NodeList s = doc.getElementsByTagName(AUTH_HEADER);
+			NodeList headNodes = s.item(0).getChildNodes();
+			for (int i = 0; i < headNodes.getLength(); i++) {
+				headNodes.item(i).setTextContent("***");
+			}
+			DOMSource domSource = new DOMSource(doc);
+			StringWriter writer = new StringWriter();
+			StreamResult result = new StreamResult(writer);
+			TransformerFactory tf = TransformerFactory.newInstance();
+			Transformer transformer = tf.newTransformer();
+			transformer.transform(domSource, result);
+			return writer.toString();
+
+		}
 	}
+
 }
